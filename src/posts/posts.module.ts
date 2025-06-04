ad5d74dc880f7952fb8f03ec47080a72a25fc0b7
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-06-04 19:49:01 +0000

    add nestjs-prisma package

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
-import { PrismaService } from '../prisma.service';
 
 @Module({
-  providers: [PostsResolver, PostsService, PrismaService],
+  providers: [PostsResolver, PostsService],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from 'nestjs-prisma';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 
