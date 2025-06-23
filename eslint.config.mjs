45801d8da90fc3206faa36aa2dcd15126bfe7a45
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2025-06-23 14:38:15 +0000

    eslint and docker

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,10 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'src/@generated'
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -28,7 +31,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn'
     },
   },
 );
