346ded26cc4e191af35af7f472eefce5774641a4
Hammed Anuoluwapo Pelumi <hammedanuoluwapopelumi@gmail.com>
2025-07-03 10:10:55 +0000

    added graphql module

---
+++ apps/api-gateway/src/gql/gql.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'apps/api-gateway/schema.gql'),
+      sortSchema: true,
+      introspection: true,
+      subscriptions: {
+        'graphql-ws': {
+          path: '/graphql',
+        },
+      },
+      graphiql: true,
+    }),
+  ],
+})
+export class GqlModule { }
