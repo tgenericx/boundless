1a0d6e1f8877f1d95a89cac390003352173e3136
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-07-16 00:43:44 +0000

    clean up

--- libs/prisma-service/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './lib/prisma-service';
-export * from './lib/custom-logger';
-export * from './lib/exception.handler';
-export * from './lib/rpc-exception.filter';
-export * from './lib/gql-exception.filter';
-export * from './lib/http-exception.filter';
-export * from './graphql-types';
-export { Prisma } from './lib/@generated/prisma/client';
