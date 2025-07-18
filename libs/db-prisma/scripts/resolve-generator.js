5e63f8eb325a917d97a7a410aa56c11a5682e1d9
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-07-18 14:28:10 +0000

    resolve gql types generator path

---
+++ libs/db-prisma/scripts/resolve-generator.js
@@ -0,0 +1,7 @@
+import * as path from 'path';
+const generatorPath = path.resolve(
+  __dirname,
+  '../node_modules/prisma-nestjs-graphql',
+);
+console.error('Using generator path:', generatorPath);
+console.log(`node ${generatorPath}`);
