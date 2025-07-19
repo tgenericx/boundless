9b3dad0c601d5d593b049660e3b8568f544b6294
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-07-19 17:24:21 +0000

    remove resolver script

--- libs/schema/scripts/resolve-generator.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const generatorPath = path.resolve(
-  __dirname,
-  '../node_modules/prisma-nestjs-graphql',
-);
-console.error('Using generator path:', generatorPath);
-console.log(generatorPath);
