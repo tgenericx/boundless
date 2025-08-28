14eee528a9320f7e1d0b1ab3b3d9f4e0f0d86bab
Anuoluwapo Pelumi Hammed <hammedanuoluwapopelumi@gmail.com>
2025-08-28 20:30:40 +0000

    feat(users): add GraphQL resolver for user operations
    - Create UsersResolver with GraphQL decorators
    - Implement query to fetch single user by unique criteria
    - Add mutation to update user information
    - Implement mutation to remove users
    - Create query to fetch multiple users with filtering options
    - Use generated GraphQL types for type safety

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,33 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import {
+  FindManyUserArgs,
+  FindUniqueUserArgs,
+  UpdateOneUserArgs,
+  User,
+} from 'src/@generated/graphql';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => User, { name: 'user' })
+  findOne(@Args() args: FindUniqueUserArgs) {
+    return this.usersService.findOne(args);
+  }
+
+  @Mutation(() => User)
+  updateUser(@Args() args: UpdateOneUserArgs) {
+    return this.usersService.update(args);
+  }
+
+  @Mutation(() => User)
+  removeUser(@Args() args: FindUniqueUserArgs) {
+    return this.usersService.remove(args);
+  }
+
+  @Query(() => [User], { name: 'users' })
+  findMany(@Args() args: FindManyUserArgs) {
+    return this.usersService.findMany(args);
+  }
+}
