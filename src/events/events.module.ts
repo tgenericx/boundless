49c28959d145e5f94272e320ca07602ba00c2e39
root <root@localhost.localdomain>
2025-11-18 19:25:20 +0000

    update event module providers

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EventsResolver } from './events.resolver';
 import { EventsService } from './events.service';
 import { EventPermission } from './events.permission';
+import { EventRepository } from './events.repository';
 
 @Module({
-  providers: [EventsResolver, EventsService, EventPermission],
+  providers: [EventsResolver, EventsService, EventPermission, EventRepository],
   exports: [EventsService],
 })
 export class EventsModule {}
